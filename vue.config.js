637d7e777cfa292410059ce2556350d8bfb055c1
Quentin AUBRY <aubryquentin700@gmail.com>
2022-03-22 23:51:07 -0400

    feat: pwa support ?

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'Deck-o-Magic',
+    themeColor: '#311b92',
+    msTileColor: '#222',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
+      // ...other Workbox options...
+    }
+  }
 })
