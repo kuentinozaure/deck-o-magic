fee0041accfd473392e05b304261b7414782af12
Quentin AUBRY <aubryquentin700@gmail.com>
2022-03-22 23:54:13 -0400

    fix: pwa

--- vue.config.js
+++ vue.config.js
@@ -6,14 +6,6 @@ module.exports = defineConfig({
     themeColor: '#311b92',
     msTileColor: '#222',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
-
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'dev/sw.js',
-      // ...other Workbox options...
-    }
+    appleMobileWebAppStatusBarStyle: 'black',    
   }
 })
