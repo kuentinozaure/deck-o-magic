58c3dc0b8706bf9cef77c14812a223706c493854
Quentin AUBRY <aubryquentin700@gmail.com>
2022-03-23 00:10:52 -0400

    fix: pwa check

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  pwa: {
-    name: 'Deck-o-Magic',
-    themeColor: '#311b92',
-    msTileColor: '#222',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',    
-  }
 })
